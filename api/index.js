a79a46a43eea198eb9b0638962f996eb8e8ca131
Karanbohara01 <karanbohara216@gmail.com>
2025-09-14 13:17:33 +0545

    jpt

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error('Could not connect to MongoDB:', err));
 
 // Routes
-app.use('/api/tasks', taskRoutes);
+app.use('/tasks', taskRoutes);
 if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server is running locally on port ${PORT}`));
